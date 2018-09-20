020ed7eeed9bd453dbc92c071d4780611f040194
paulohrodriguesaifal <paulohrodriguesaifal@gmail.com>
2018-09-20 16:39:05 -0300

    3

--- main2.py
+++ main2.py
@@ -7,7 +7,24 @@ cred = credentials.Certificate('aula1-c93f4-firebase-adminsdk-ib3cp-82af23ef77.j
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
-db.collection("products").document("123").set({
-    "nome":"arroz",
-    "codigo":"12371892"
-});
+# db.collection("products").document("123").set({
+#     "nome":"arroz",
+#     "codigo":"12371892"
+# });
+
+# docs = db.collection("products").get()
+# for doc in docs:
+#     print("{} => {}".format(doc.id,doc.to_dict()))
+
+dicionario = {}
+
+print("digite o nome do produto")
+dicionario["nome"] = input()
+
+print("digite um codigo para o produto")
+dicionario["codigo"] = input()
+
+print("digite o preco do produto")
+dicionario["preco"] = input()
+
+db.collection("products").document("123").set(dicionario);
