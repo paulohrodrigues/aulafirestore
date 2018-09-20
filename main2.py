e5c948b2fe842dd6f0a5caeb2473209056c46fb6
paulohrodriguesaifal <paulohrodriguesaifal@gmail.com>
2018-09-20 16:23:58 -0300

    2

--- main2.py
+++ main2.py
@@ -1,3 +1,4 @@
+# https://github.com/paulohrodrigues/aulafirestore
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
@@ -5,3 +6,8 @@ from firebase_admin import firestore
 cred = credentials.Certificate('aula1-c93f4-firebase-adminsdk-ib3cp-82af23ef77.json')
 firebase_admin.initialize_app(cred)
 db = firestore.client()
+
+db.collection("products").document("123").set({
+    "nome":"arroz",
+    "codigo":"12371892"
+});
