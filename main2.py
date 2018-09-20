1f8f85c6b3d460d77681450a32b1660966587f9f
paulohrodriguesaifal <paulohrodriguesaifal@gmail.com>
2018-09-20 17:53:48 -0300

    merge

--- main2.py
+++ main2.py
@@ -1,9 +1,8 @@
-# https://github.com/paulohrodrigues/aulafirestore
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-cred = credentials.Certificate('aula1-c93f4-firebase-adminsdk-ib3cp-82af23ef77.json')
+cred = credentials.Certificate('nomeDaSuaChave.json')
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
